e0c757a42c045439fdacac1ed6a1f7efb4b72099
Bart Oldeman <bart.oldeman@calculquebec.ca>
2024-03-19 13:08:39 -0400

    Use last X field numbers for stats.
    AMD xhpl inserts a field for SWP

--- hpl-cpu/run.sh
+++ hpl-cpu/run.sh
@@ -9,7 +9,7 @@ else
 fi
 
 PASSED=$(grep -E '_oo/' hpl.log | awk '{print $4}')
-RESULT=$(grep -A 2 -E 'Time *Gflops' hpl.log | tail -n 1 | awk '{print $2","$3","$4","$5","$6","$7}')
+RESULT=$(grep -A 2 -E 'Time *Gflops' hpl.log | tail -n 1 | awk '{print $(NF-5)","$(NF-4)","$(NF-3)","$(NF-2)","$(NF-1)","$NF}')
 
 echo Test,Hostname,Timestamp,ResidualCheck,N,NB,P,Q,ElapsedTime,Gflops
 echo HPL-CPU,$(hostname),$(date '+%F %T'),$PASSED,$RESULT
