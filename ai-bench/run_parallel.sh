a0439607b9852905b7398352f97d03bccba81a2a
Jean-Francois St-Pierre <jf.stpierre@calculquebec.ca>
2024-03-24 17:27:55 -0400

    Changes to move to mpiexec launche for parallel ai bench

--- ai-bench/run_parallel.sh
+++ ai-bench/run_parallel.sh
@@ -16,8 +16,7 @@ MAIN_NODE=$1
 MAIN_PORT=$2
 N_NODES=$3
 N_GPUS=$4
-MACHINE_RANK=$5
-
+MACHINE_RANK=$OMPI_COMM_WORLD_RANK
 run_bench_parallel(){
 
    cd $BASE_DIR/benchmarks/$1
@@ -27,11 +26,11 @@ run_bench_parallel(){
 
        if [ "$1" == "large_language_model" ]; then
 
-          accelerate launch --mixed_precision=fp16 --num_machines=${N_NODES} --num_processes=${N_PROCS} --main_process_ip=${MAIN_NODE} --main_process_port=${MAIN_PORT} --machine_rank=${MACHINE_RANK} --config_file="${BASE_DIR}/configs/fsdp_llama.yaml"  main.py
+          accelerate launch  --mixed_precision=fp16 --num_machines=${N_NODES} --num_processes=${N_PROCS} --main_process_ip=${MAIN_NODE} --main_process_port=${MAIN_PORT} --machine_rank=${MACHINE_RANK} --config_file="${BASE_DIR}/configs/fsdp_llama.yaml"  main.py
 
        else
 
-          accelerate launch --mixed_precision=fp16 --num_machines=${N_NODES} --num_processes=${N_PROCS} --main_process_ip=${MAIN_NODE} --main_process_port=${MAIN_PORT} --machine_rank=${MACHINE_RANK} main.py
+          accelerate launch  --mixed_precision=fp16 --num_machines=${N_NODES} --num_processes=${N_PROCS} --main_process_ip=${MAIN_NODE} --main_process_port=${MAIN_PORT} --machine_rank=${MACHINE_RANK} main.py
 
        fi
 
@@ -40,6 +39,7 @@ run_bench_parallel(){
 
 echo "Starting benchmark suite..."
 
+#BENCHMARKS="large_language_model" #`ls $BASE_DIR/benchmarks`
 BENCHMARKS=`ls $BASE_DIR/benchmarks`
 
 for bench in $BENCHMARKS
